c3c5c208cb4d4c0d00e25e95626e0ccbd87a0c39
Nick Verdin <nv1028@aggienetwork.com>
2020-06-15 14:28:21 -0500

    added oop

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,26 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return "vroooom"
+            # any defined method needs to have self passed into it
     # TODO
+    
+#technically should be:
+# class GroundVehicle(Vehicle):
+#     def __init__(self, num_wheels = 4):
+#         self.num_wheels = num_wheels
+#         super().__init__(depends on Vehicle)
+
+#     def drive(self):
+#         return "vroooom"
+            # any defined method needs to have self passed into it
+    
+
+
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,7 +32,14 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels = 2)
 
+    def drive(self):
+        return "BRAAAP!!"
+        #return f'vroooom'
+            # any defined method needs to have self passed into it
 # TODO
 
 vehicles = [
@@ -29,4 +52,9 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
+print(vehicles[0].drive())
+print(vehicles[1].drive())
+print(vehicles[2].drive())
+print(vehicles[3].drive())
+print(vehicles[4].drive())
 # TODO
